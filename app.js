0d0d293af07787caba05ebe48803967f73133fd8
Santiago Alonso <santialonso13@gmail.com>
2023-12-04 19:16:57 -0300

    segunda preentrega

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-const SI = 'si'; 
-const NO = 'no'; 
+const productos = [new producto('Leche La Serenisima', 'Leche entera La Serenisima', 150, 'Almacen'),
+                   new producto('Mermelada BC', 'Mermelada dietetica sabor frutilla', 300, 'Almacen'),  
+                   new producto('Pan', 'Tira de pan fresca', 50, 'Almacen'), 
+                   new producto('Manteca Sancor','Manteca marca Sancor', 100, 'Almacen'), 
+                   new producto('Papa', 'Bolsa de 2kg de papa', 150, 'Verduleria'), 
+                   new producto('Carnes', 'Carnes Vacuna por kg', 800, 'Carniceria'),
+]; 
+const iniciob = [   new inicio('Carniceria'),
+                    new inicio('Verduleria'),
+                    new inicio('Almacen'),
 
-let nombre;  
-let producto; 
-let precio;  
-let cantidad; 
-let total=0; 
-let continuar; 
-let detalle;
-let ticket = 'Producto      PrecioUnitario      Cantidad        Importe  \n'; 
+];
+const ticket = [];   
 
-nombre = prompt('Bienvenido a la Tienda Virtual de Carniceria Los Montes... \n Ingrese su Nombre o Usuario: ');  
-do{ 
-    producto = prompt('Ingrese producto:  ');   
-    precio = prompt('Ingrese el PrecioUnitario:  '); 
-    cantidad = prompt('Ingrese la cantidad de productos que desea: ');  
-    detalle = `${producto}  ${precio}  ${cantidad}  ${precio*cantidad} \n`;  
-    ticket = ticket + detalle; 
-    total = total + precio * cantidad; 
+function bienvenidos(){ 
+    let menub = `Tienda Virtual Carniceria Los Montes: \n 
+        Ingrese la Categoria que desea visitar: \n`;   
+    iniciob.forEach((inicio,index) => {  
+        menub = menub +`${index+1} - ${inicio.categoria} \n`
 
+    }
+    ); 
+ 
+   let opcion = prompt(menub);  
+    while (opcion < 1 || opcion > menub.length){  
+   opcion = prompt('Categoria Invalida, Ingrese una Categoria especifica: ')
 
-    continuar = prompt('Desea seguir comprando? si/no').toLowerCase(); 
+}
+}
+
+function almacen(){  
+    let pedidoalmacen = `Tienda Virtual Carniceria Los Montes \n `  ;  
+    productos.forEach((producto,index) => { 
+        pedidoalmacen = pedidoalmacen +  
+        `${index+1} - ${producto.nombre} : ${producto.descripcion} \n
+                precio: ${producto.precio} \n ` ;
+        
+    });
+
+let opcion = prompt(pedidoalmacen);  
+while (opcion < 1 || opcion > productos.length){  
+   opcion = prompt(ticket + 'Producto Invalido, Ingrese un nuevo producto: ')
+
+}
+ticket.push(productos[opcion - 1]); 
+}  
+
+function cerrarticket(){ 
+   let total = ticket.reduce((total,producto) => {return total + producto.precio}, 0);  
+   alert (`Su ticket es de: $${total}`);
+
+}
+
+function menuprincipal(){  
+    let opcion;
+    do{ 
+        almacen(); 
+        opcion = prompt('Desea seguir comprando? SI/NO');
+    }  
+    while(opcion !== 'NO');
 } 
-while(continuar === SI); 
-
-ticket = ticket + `TOTAL: $${total}`; 
-alert(ticket); 
-alert('GRACIAS POR SU COMPRA');
+bienvenidos();
+menuprincipal(); 
+cerrarticket();  
---
+++ productos.js
@@ -0,0 +1,22 @@
+class producto { 
+    nombre;
+    descripcion; 
+    precio;
+
+
+    constructor(nombre,descripcion,precio,) { 
+        this.nombre = nombre; 
+        this.descripcion = descripcion; 
+        this.precio = precio;  
+        
+    }
+
+} 
+
+class inicio { 
+    categoria; 
+    constructor(categoria){ 
+        this.categoria = categoria;
+    }
+} 
+
